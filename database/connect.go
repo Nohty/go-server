6e3e3dcc121f5ce5d2cd08f5d66d8fdc8537f727
Daan Hautekiet <daanhautekiet@gmail.com>
2024-01-11 16:48:34 +0100

    Update database logger level

--- database/connect.go
+++ database/connect.go
@@ -12,7 +12,7 @@ import (
 func ConnectDB() {
 	var err error
 	DB, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Silent),
+		Logger: logger.Default.LogMode(logger.Error),
 	})
 
 	if err != nil {
