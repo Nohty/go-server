53abc9965407d4b431956aa3df295e0812581bd1
Daan Hautekiet <daanhautekiet@gmail.com>
2024-01-11 19:09:58 +0100

    Add TODO's

--- router/router.go
+++ router/router.go
@@ -24,4 +24,14 @@ func SetupRoutes(app *fiber.App) {
 	user.Put("/:id<int>", middleware.Protected(), handler.UpdateUser)
 	user.Delete("/:id<int>", middleware.Protected(), handler.DeleteUser)
 	user.Put("/:id<int>/password", middleware.Protected(), handler.UpdatePassword)
+
+	// Delivery
+	// delivery := api.Group("/delivery")
+	// TODO: delivery.Get("/", middleware.Protected(), handler.GetDeliveries)
+	// TODO: delivery.Get("/:id<int>", middleware.Protected(), handler.GetDelivery)
+	// TODO: delivery.Post("/", middleware.Protected(), handler.CreateDelivery)
+	// TODO: delivery.Put("/:id<int>", middleware.Protected(), handler.UpdateDelivery)
+	// TODO: delivery.Delete("/:id<int>", middleware.Protected(), handler.DeleteDelivery)
+	// TODO: delivery.Put("/:id<int>/status", middleware.Protected(), handler.UpdateDeliveryStatus)
+	// TODO: delivery.Post("/:id<int>/pay", middleware.Protected(), handler.PayDelivery)
 }
