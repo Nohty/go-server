45566b9948ece11986fe393fb8cac34a7cf61668
Daan Hautekiet <daanhautekiet@gmail.com>
2024-01-11 17:55:22 +0100

    Update User model with new imports and PermissionFlags

--- model/user.go
+++ model/user.go
@@ -1,15 +1,18 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"github.com/Nohty/api/utils"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model `json:"-"`
-	Username   string  `gorm:"uniqueIndex;not null" json:"username"`
-	Email      string  `gorm:"uniqueIndex;not null" json:"email"`
-	Phone      string  `gorm:"not null" json:"phone"`
-	Password   string  `gorm:"not null" json:"-"`
-	Permission int64   `gorm:"not null" json:"permission"`
-	WalletAddr string  `gorm:"not null" json:"wallet_addr"`
-	Address    Address `gorm:"foreignKey:ID" json:"address"`
-	Contacts   []User  `gorm:"many2many:user_contacts;" json:"contacts"`
+	Username   string                `gorm:"uniqueIndex;not null" json:"username"`
+	Email      string                `gorm:"uniqueIndex;not null" json:"email"`
+	Phone      string                `gorm:"not null" json:"phone"`
+	Password   string                `gorm:"not null" json:"-"`
+	Permission utils.PermissionFlags `gorm:"not null" json:"permission"`
+	WalletAddr string                `gorm:"not null" json:"wallet_addr"`
+	Address    Address               `gorm:"foreignKey:ID" json:"address"`
+	Contacts   []User                `gorm:"many2many:user_contacts;" json:"contacts"`
 }
