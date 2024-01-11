2e589fcd56dfa13280f1c3dc4374eb250edc29b3
Daan Hautekiet <daanhautekiet@gmail.com>
2024-01-11 16:49:13 +0100

    Add user handler functions

---
+++ handler/user.go
@@ -0,0 +1,76 @@
+package handler
+
+import (
+	"github.com/Nohty/api/database"
+	"github.com/Nohty/api/model"
+	"github.com/Nohty/api/utils"
+
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func hashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func GetUser(c *fiber.Ctx) error {
+	id := c.Params("id")
+	db := database.DB
+
+	var user model.User
+	db.Find(&user, id)
+
+	if user.Username == "" {
+		return utils.Response(c, fiber.StatusNotFound, "User not found", nil)
+	}
+
+	return utils.Response(c, fiber.StatusOK, "success", user)
+}
+
+func CreateUser(c *fiber.Ctx) error {
+	type NewUser struct {
+		Username   string `json:"username" validate:"required,ascii"`
+		Email      string `json:"email" validate:"required,email"`
+		Phone      string `json:"phone" validate:"required,e164"`
+		Password   string `json:"password" validate:"required,ascii,min=6"`
+		WalletAddr string `json:"wallet_addr" validate:"required,ascii"`
+		Street     string `json:"street" validate:"required,ascii"`
+		City       string `json:"city" validate:"required,ascii"`
+		Postcode   string `json:"postcode" validate:"required,ascii"`
+		Number     string `json:"number" validate:"required,ascii"`
+	}
+
+	input := new(NewUser)
+
+	if err := utils.ParseBodyAndValidate(c, input); err != nil {
+		return err
+	}
+
+	user := model.User{
+		Username:   input.Username,
+		Email:      input.Email,
+		Phone:      input.Phone,
+		WalletAddr: input.WalletAddr,
+		Address: model.Address{
+			Street:   input.Street,
+			City:     input.City,
+			Postcode: input.Postcode,
+			Number:   input.Number,
+		},
+	}
+
+	hash, err := hashPassword(input.Password)
+	if err != nil {
+		return utils.Response(c, fiber.StatusInternalServerError, "Error hashing password", nil)
+	}
+
+	user.Password = hash
+
+	db := database.DB
+	if err := db.Create(&user).Error; err != nil {
+		return utils.Response(c, fiber.StatusInternalServerError, "Error creating user", err)
+	}
+
+	return utils.Response(c, fiber.StatusCreated, "User successfully created", user)
+}
